16b6d982129358db0905750fc36612405a1ea644
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-02 18:08:36 -0500

    finished number 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) 
+{
+    let sum=0;
+    let start=0;
+    let end=0;
+    if (num1 > 0 && num2 > 0 && Number.isInteger(num1) && Number.isInteger(num2))
+    {
+        if (num1 <= num2)
+        {
+            start = num1;
+            end = num2;
+        }
+        else
+        {
+            start = num2;
+            end = num1;
+        }
+        
+        
+    for(let i=start; i <= end; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    else
+    {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
