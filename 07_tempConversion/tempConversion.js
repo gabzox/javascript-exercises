2751465191bb000f1b5f1b8e4452f86c5fccc1f7
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-02 18:34:20 -0500

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celcius) 
+{
+  return Math.round((((celcius-32)*5)/9)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(farenheit) 
+{
+  return Math.round((((farenheit*9)/5) + 32)*10)/10
 };
 
 // Do not edit below this line
