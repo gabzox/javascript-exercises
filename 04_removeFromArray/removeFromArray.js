12f9dd1b4d37903d6123e0d14d0587ede3b9e643
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-02 17:49:05 -0500

    solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, toRemove) 
+{
+    let indexToRemove;
+    for (let i=1; i<arguments.length; i++)
+    {
+        indexToRemove = list.indexOf(arguments[i]);
+        while(indexToRemove >=0)
+            {
+                list.splice(list.indexOf(arguments[i]),1);
+                indexToRemove = list.indexOf(arguments[i]);
+            }
+    }
+    return list;
 };
 
 // Do not edit below this line
