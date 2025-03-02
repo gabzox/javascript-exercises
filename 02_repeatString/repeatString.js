e6f14205d9129e7f43ac76c6682f113ed01ae9fa
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-02 15:44:18 -0500

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(originalString, num) 
+{
+    let repeatedString = "";
+    if (num>=0)
+    {
+        for (let i = 0;i<num;i++)
+        {
+            repeatedString += originalString;
+        }
 
+        return repeatedString;
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
