05e23248380428a8581e719c12fc2d533deecac3
gabzo <198239538+gabzox@users.noreply.github.com>
2025-03-02 17:17:04 -0500

    solved lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const letterInString = string.split("");
+    let newString = "";
+    
+    for (let a = (letterInString.length - 1); a >=0; a--)
+    {
+        newString += letterInString[a];
+    }
+    return newString;
 };
 
 // Do not edit below this line
